fe854dcd64ec9361245ad054805f56076aca03c3
Michael Angelo P. Fernandez <94979754+MichaelAngeloPFernandez@users.noreply.github.com>
2024-11-19 23:36:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Activities</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background: linear-gradient(to right, #28a745, #007bff); 
+            color: white; 
+        }
+        
+        ul.list-unstyled li a{
+        color: black; 
+         }
+    </style>
+</head>
+<body>
+    <h1 class="text-center">IT ELECTIVE 3</h1>
+    <h2 class="text-center">Section: 3BSIT-2</h2>
+    <hr>
+    <div class="text-center">
+    <img
+          src="pic/me.jpg"
+          class="card-img-top"
+          style="width: 150px; height: 150px"
+          alt="profile picture"
+        /> 
+    </div>
+    
+    <h4 class="text-center">Michael Angelo P. Fernandez</h4>
+    
+<div class="container text-center">
+    <h2>BOOTSTRAP ACTIVITIES</h2>
+    <ul class="list-unstyled">
+        <li><a href="Activity1.php">Activity 1</a></li>
+        <li><a href="Activity2.php">Activity 2</a></li>
+        <li><a href="Activity3.php">Activity 3</a></li>
+        <li><a href="Activity4.php">Activity 4</a></li>
+        <li><a href="Activity5.php">Activity 5</a></li>
+        <li><a href="Activity6.php">Activity 6</a></li>
+        <li><a href="Portfolio.php">My Portfolio</a></li>
+    <!--<li><a href="portfolio2.php">My Portfolio</a></li>-->
+    </ul>       
+    </div>
+</body>
+</html>
